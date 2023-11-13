82a7d6e52900dbb6ffcfa8ac6b279876b557f04c
Julieta Zaracho <zarachojuli15@gmail.com>
2023-11-13 02:13:19 -0300

    como hacer estrellas de promedio

--- frontEnd/src/app/plantillas/ratingGeneral/rating.component.ts
+++ frontEnd/src/app/plantillas/ratingGeneral/rating.component.ts
@@ -8,11 +8,25 @@ import { FormControl} from '@angular/forms'
 })
 export class RatingComponent implements OnInit{
 
+  resenias: [] = []
+
   constructor(){}
 
   ngOnInit(): void {
     
   }
+
+/* con el getAll de reseñas lleno el array resenias y se hace el promedio */
+
+/*   getCalificacionPromedio(): number {
+    if (this.resenias.length === 0) {
+      return 0; // Retorna 0 si no hay reseñas
+    }
+
+    const sumatoriaEstrellas = this.resenias.reduce((total, resenia) => total + resenia.estrellas, 0);
+    return sumatoriaEstrellas / this.resenias.length;
+  } */
+
 
 
 }
